89413e6ac3be335f4c4db72c2b44941aea8e12ef
Affandi09 <48713631+Affandi09@users.noreply.github.com>
2021-04-02 17:05:29 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const qs = require('querystring');
 const axios = require('axios');
-var password = "fdciabdul123";
+var password = "@akmj09";
 const fs = require("fs");
-var user = "fakyuanjing";
+var user = "genshinakmj";
 var n = 10; 
 for(var i=1; i<=n; i++){
   var username = user + n++;
